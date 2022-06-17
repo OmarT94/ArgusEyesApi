d11cd6bcb08543b5d05665af4a87e08d257b0353
Omar Tamr <omrtmr94@gmail.com>
2022-06-17 17:04:09 +0200

    Projektdateien hinzufügen.

---
+++ Controllers/ArgusController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArgusEyesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArgusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var image = System.IO.File.OpenRead(@"C:\\Users\Omar\source\repos\ArgusEyesApi\wwwroot\Arguseyes\retina.jpg");
+            var result = File(image, "image/jpeg");
+            return result;
+        }
+    }
+}
+//https://localhost:7133/api/Argus
